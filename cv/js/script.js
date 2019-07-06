abe9196a1fedd0bf1dbd0fca878ed2d9ace35873
Johan Wänglöf <jwanglof@gmail.com>
2019-07-06 16:21:18 +0200

    New welcome message and added SBAB as experience

--- cv/js/script.js
+++ cv/js/script.js
@@ -1,1 +1,13 @@
-console.log('Hi, and welcome to my Curriculum Vitae! If you have any questions or remarks, please don\'t hesitate to contact me!');
+console.log(
+  ' __ __    ___  _      _       ___      \n'
+  + '|  |  |  /  _]| |    | |     /   \\     \n'
+  + '|  |  | /  [_ | |    | |    |     |    \n'
+  + '|  _  ||    _]| |___ | |___ |  O  |    \n'
+  + '|  |  ||   [_ |     ||     ||     | __ \n'
+  + '|  |  ||     ||     ||     ||     ||  |\n'
+  + '|__|__||_____||_____||_____| \\___/ |_ |\n'
+  + '                                     \\|'
+  + '\n'
+  + 'and welcome to my Curriculum Vitae! If you have any questions or remarks, please don\'t hesitate to contact me!\n'
+  + 'You find my contact information on the top of the page :)'
+);
